223d49718381159c46f40222a6e8b0343b6eb3c2
jules fabien <julesfabien96@gmail.com>
2023-02-22 16:39:00 +0200

    Integrate Dashboard

---
+++ resources/views/partials/menus.blade.php
@@ -0,0 +1,56 @@
+<ul class="menu-nav">
+    <li class="menu-item nav-dashboard" aria-haspopup="true">
+        <a href="/" class="menu-link">
+            <i class="menu-icon flaticon-dashboard"></i>
+            <span class="menu-text">Dashboard</span>
+        </a>
+    </li>
+{{--    @can('Manage System Users')--}}
+{{--        <li class="menu-section">--}}
+{{--            <h4 class="menu-text">System Users Section</h4>--}}
+{{--            <i class="menu-icon ki ki-bold-more-hor icon-md"></i>--}}
+{{--        </li>--}}
+{{--        <li class="menu-item menu-item-submenu nav-user-managements" aria-haspopup="true" data-menu-toggle="hover">--}}
+{{--            <a href="javascript:;" class="menu-link menu-toggle">--}}
+{{--                <i class="menu-icon flaticon-users"></i>--}}
+{{--                <span class="menu-text">User Management</span>--}}
+{{--                <i class="menu-arrow"></i>--}}
+{{--            </a>--}}
+{{--            <div class="menu-submenu">--}}
+{{--                <i class="menu-arrow"></i>--}}
+{{--                <ul class="menu-subnav">--}}
+{{--                    <li class="menu-item menu-item-parent" aria-haspopup="true">--}}
+{{--                            <span class="menu-link">--}}
+{{--                                <span class="menu-text">User Management</span>--}}
+{{--                            </span>--}}
+{{--                    </li>--}}
+{{--                    <li class="menu-item nav-all-users" aria-haspopup="true">--}}
+{{--                        <a href="{{ route('admin.users.index') }}" class="menu-link">--}}
+{{--                            <i class="menu-bullet menu-bullet-dot">--}}
+{{--                                <span></span>--}}
+{{--                            </i>--}}
+{{--                            <span class="menu-text">Users</span>--}}
+{{--                        </a>--}}
+{{--                    </li>--}}
+{{--                    <li class="menu-item nav-roles" aria-haspopup="true">--}}
+{{--                        <a href="{{ route('admin.roles.index') }}" class="menu-link">--}}
+{{--                            <i class="menu-bullet menu-bullet-dot">--}}
+{{--                                <span></span>--}}
+{{--                            </i>--}}
+{{--                            <span class="menu-text">Roles</span>--}}
+{{--                        </a>--}}
+{{--                    </li>--}}
+{{--                    <li class="menu-item nav-all-permissions" aria-haspopup="true">--}}
+{{--                        <a href="{{ route('admin.permissions.index') }}" class="menu-link">--}}
+{{--                            <i class="menu-bullet menu-bullet-dot">--}}
+{{--                                <span></span>--}}
+{{--                            </i>--}}
+{{--                            <span class="menu-text">Permissions</span>--}}
+{{--                        </a>--}}
+{{--                    </li>--}}
+{{--                </ul>--}}
+{{--            </div>--}}
+{{--        </li>--}}
+{{--    @endcan--}}
+</ul>
+
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
+    Route::get('/dashboard', [\App\Http\Controllers\DashboardController::class,'index'])->name('dashboard');
+    Route::get("/users/profile/{user_id}", [App\Http\Controllers\UserController::class, 'userProfile'])->name("users.profile");
+});
+
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
