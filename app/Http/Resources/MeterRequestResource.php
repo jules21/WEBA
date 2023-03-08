d3b58da50b0de747523dd9cb5687cf732bdabf73
frank ricardo <frankricardo35@gmail.com>
2023-03-08 17:04:45 +0200

    commit

---
+++ app/Http/Resources/MeterRequestResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MeterRequestResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'=>$this->id,
+            'subscription_number'=>$this->subscription_number,
+            'meter_number'=>$this->meter_number,
+            'status'=>$this->status,
+            'last_index'=>$this->last_index,
+            'balance'=>$this->balance,
+            'customer'=>$this->request->customer,
+        ];
+    }
+}
