db36ed0485c77f11fcee606d35081a84a6b782f6
CaMi <jeanpaulcami@live.com>
2023-03-13 19:13:58 +0200

    fix billing charges

--- app/Http/Controllers/ChartAccountController.php
+++ app/Http/Controllers/ChartAccountController.php
@@ -8,79 +8,15 @@ use App\Models\ChartAccount;
 
 class ChartAccountController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        $chartAccounts = ChartAccount::query()
+            ->where('operation_area_id', '=', auth()->user()->operation_area)
+            ->whereNull('parent_ledger_no')
+            ->with('children.children')
+//            ->orderBy('code')
+            ->get();
+        return view('admin.accounting.chart_of_accounts', compact('chartAccounts'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreChartAccountRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreChartAccountRequest $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\ChartAccount  $chartAccount
-     * @return \Illuminate\Http\Response
-     */
-    public function show(ChartAccount $chartAccount)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\ChartAccount  $chartAccount
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(ChartAccount $chartAccount)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateChartAccountRequest  $request
-     * @param  \App\Models\ChartAccount  $chartAccount
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateChartAccountRequest $request, ChartAccount $chartAccount)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\ChartAccount  $chartAccount
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(ChartAccount $chartAccount)
-    {
-        //
-    }
 }
