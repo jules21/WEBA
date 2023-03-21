7be1655d53106275e59d1b94418d77e0ec39a71b
jules fabien <julesfabien96@gmail.com>
2023-03-21 15:44:21 +0200

    fix

--- resources/views/partials/_alerts.blade.php
+++ resources/views/partials/_alerts.blade.php
@@ -68,3 +68,24 @@
         </div>
     </div>
 @endif
+
+@if($errors->any())
+    @foreach($errors->all() as $error)
+        <div
+            class="alert alert-custom alert-notice  my-3 alert-outline-danger bg-white fade show rounded-sm"
+            role="alert">
+            <div class="alert-icon">
+                <i class="flaticon-information"></i>
+            </div>
+            <div class="alert-text">
+                <span>{{ $error }}</span>
+            </div>
+            <div class="alert-close">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true"><i class="ki ki-close"></i></span>
+                </button>
+            </div>
+        </div>
+
+    @endforeach
+@endif
