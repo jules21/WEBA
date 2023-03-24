fbc155cc13bf789fe594da3a39c1afdbda6a07be
CaMi <jeanpaulcami@live.com>
2023-03-24 14:37:55 +0200

    add pipe cross types

--- app/Models/RequestPipeCross.php
+++ app/Models/RequestPipeCross.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class RequestPipeCross extends Model
 {
-    use HasFactory;
+    public function request(): BelongsTo
+    {
+        return $this->belongsTo(Request::class);
+    }
+
+    public function pipeCross(): BelongsTo
+    {
+        return $this->belongsTo(RoadCrossType::class, 'road_cross_type_id');
+    }
 }
