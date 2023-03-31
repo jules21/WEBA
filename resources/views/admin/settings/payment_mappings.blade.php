414077dc5af08af4ff8b8a4e213038e058bc2dce
Jabo Jean Josue <jabojeanjosue@gmail.com>
2023-03-31 15:01:33 +0200

    some changes on settings menu

--- resources/views/admin/settings/payment_mappings.blade.php
+++ resources/views/admin/settings/payment_mappings.blade.php
@@ -39,9 +39,9 @@
             </div>
             <div class="card-toolbar">
                 <!-- Button trigger modal-->
-                <button type="button" class="btn btn-primary" data-toggle="modal"
+                <button type="button" class="btn btn-light-primary" data-toggle="modal"
                         data-target="#exampleModalLong">
-                    <span class="flaticon-add"></span>
+                    <i class="flaticon2-plus"></i>
                     Add New Record
                 </button>
 
@@ -76,7 +76,7 @@
                             <td>{{$mapping->created_at}}</td>
                             <td>
                                 <div class="dropdown">
-                                    <button class="btn btn-primary btn-sm dropdown-toggle" type="button"
+                                    <button class="btn btn-light-primary btn-sm dropdown-toggle" type="button"
                                             id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
                                             aria-expanded="false">
                                         Action
