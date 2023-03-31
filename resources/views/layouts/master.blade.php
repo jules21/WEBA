a0bee70f503461c80c33906e5dfde7df3365dfc5
CaMi <jeanpaulcami@live.com>
2023-03-31 08:12:26 -0400

    Start create purchase change

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -136,17 +136,21 @@
                     <!--begin::Header Menu Wrapper-->
                     <div class="header-menu-wrapper header-menu-wrapper-left" id="kt_header_menu_wrapper">
                         <!--begin::Header Menu-->
-                        <div id="kt_header_menu" class="header-menu header-menu-mobile header-menu-layout-default">
+                        <div id="kt_header_menu" class="header-menu header-menu-mobile py-md-4 py-20 header-menu-layout-default">
                             <!--begin::Header Nav-->
                             <ul class="menu-nav">
                                 <li class="menu-item menu-item-submenu menu-item-rel menu-item-active"
                                     data-menu-toggle="click" aria-haspopup="true">
                                     <a href="javascript:void(0);" class="">
-                                        <span class="menu-text text-muted" style="font-size: 22px;">
+                                        <span class="menu-text text-muted" style="font-size: 1.6rem">
                                             <span> CMS RWSS System</span>
                                             @if(Helper::isOperator() || Helper::hasOperationArea())
                                                 | <small
-                                                    class="ml-3 text-primary rounded-sm badge bg-light-primary">{{auth()->user()->operator->name ?? ""}}</small>
+                                                    class="ml-1 p-2 text-primary rounded-sm badge bg-light-primary">{{auth()->user()->operator->name ?? ""}}</small>
+                                            @endif
+                                            @if(Helper::hasOperationArea())
+                                                | <small
+                                                    class="ml-1 p-2 text-info rounded-sm badge bg-light-info">{{auth()->user()->operationArea->name ?? ""}}</small>
                                             @endif
                                         </span>
                                         <i class="menu-arrow"></i>
@@ -330,18 +334,24 @@
 
                 </div>
 
-            </a>            <a href="{{route("admin.users.permissions",encryptId(Auth::user()->id))}}"
-               class="navi-item mb-5">
+            </a> <a href="{{route("admin.users.permissions",encryptId(Auth::user()->id))}}"
+                    class="navi-item mb-5">
                 <div class="navi-link">
                     <div class="symbol symbol-40 bg-light mr-3">
                         <div class="symbol-label">
 									<span class="svg-icon svg-icon-md svg-icon-success">
 										<!--begin::Svg Icon | path:assets/media/svg/icons/General/Notification2.svg-->
-                                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                        <svg xmlns="http://www.w3.org/2000/svg"
+                                             xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px"
+                                             viewBox="0 0 24 24" version="1.1">
 											<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
 												<polygon points="0 0 24 0 24 24 0 24"></polygon>
-												<path d="M4.85714286,1 L11.7364114,1 C12.0910962,1 12.4343066,1.12568431 12.7051108,1.35473959 L17.4686994,5.3839416 C17.8056532,5.66894833 18,6.08787823 18,6.52920201 L18,19.0833333 C18,20.8738751 17.9795521,21 16.1428571,21 L4.85714286,21 C3.02044787,21 3,20.8738751 3,19.0833333 L3,2.91666667 C3,1.12612489 3.02044787,1 4.85714286,1 Z M8,12 C7.44771525,12 7,12.4477153 7,13 C7,13.5522847 7.44771525,14 8,14 L15,14 C15.5522847,14 16,13.5522847 16,13 C16,12.4477153 15.5522847,12 15,12 L8,12 Z M8,16 C7.44771525,16 7,16.4477153 7,17 C7,17.5522847 7.44771525,18 8,18 L11,18 C11.5522847,18 12,17.5522847 12,17 C12,16.4477153 11.5522847,16 11,16 L8,16 Z" fill="#000000" fill-rule="nonzero" opacity="0.3"></path>
-												<path d="M6.85714286,3 L14.7364114,3 C15.0910962,3 15.4343066,3.12568431 15.7051108,3.35473959 L20.4686994,7.3839416 C20.8056532,7.66894833 21,8.08787823 21,8.52920201 L21,21.0833333 C21,22.8738751 20.9795521,23 19.1428571,23 L6.85714286,23 C5.02044787,23 5,22.8738751 5,21.0833333 L5,4.91666667 C5,3.12612489 5.02044787,3 6.85714286,3 Z M8,12 C7.44771525,12 7,12.4477153 7,13 C7,13.5522847 7.44771525,14 8,14 L15,14 C15.5522847,14 16,13.5522847 16,13 C16,12.4477153 15.5522847,12 15,12 L8,12 Z M8,16 C7.44771525,16 7,16.4477153 7,17 C7,17.5522847 7.44771525,18 8,18 L11,18 C11.5522847,18 12,17.5522847 12,17 C12,16.4477153 11.5522847,16 11,16 L8,16 Z" fill="#000000" fill-rule="nonzero"></path>
+												<path
+                                                    d="M4.85714286,1 L11.7364114,1 C12.0910962,1 12.4343066,1.12568431 12.7051108,1.35473959 L17.4686994,5.3839416 C17.8056532,5.66894833 18,6.08787823 18,6.52920201 L18,19.0833333 C18,20.8738751 17.9795521,21 16.1428571,21 L4.85714286,21 C3.02044787,21 3,20.8738751 3,19.0833333 L3,2.91666667 C3,1.12612489 3.02044787,1 4.85714286,1 Z M8,12 C7.44771525,12 7,12.4477153 7,13 C7,13.5522847 7.44771525,14 8,14 L15,14 C15.5522847,14 16,13.5522847 16,13 C16,12.4477153 15.5522847,12 15,12 L8,12 Z M8,16 C7.44771525,16 7,16.4477153 7,17 C7,17.5522847 7.44771525,18 8,18 L11,18 C11.5522847,18 12,17.5522847 12,17 C12,16.4477153 11.5522847,16 11,16 L8,16 Z"
+                                                    fill="#000000" fill-rule="nonzero" opacity="0.3"></path>
+												<path
+                                                    d="M6.85714286,3 L14.7364114,3 C15.0910962,3 15.4343066,3.12568431 15.7051108,3.35473959 L20.4686994,7.3839416 C20.8056532,7.66894833 21,8.08787823 21,8.52920201 L21,21.0833333 C21,22.8738751 20.9795521,23 19.1428571,23 L6.85714286,23 C5.02044787,23 5,22.8738751 5,21.0833333 L5,4.91666667 C5,3.12612489 5.02044787,3 6.85714286,3 Z M8,12 C7.44771525,12 7,12.4477153 7,13 C7,13.5522847 7.44771525,14 8,14 L15,14 C15.5522847,14 16,13.5522847 16,13 C16,12.4477153 15.5522847,12 15,12 L8,12 Z M8,16 C7.44771525,16 7,16.4477153 7,17 C7,17.5522847 7.44771525,18 8,18 L11,18 C11.5522847,18 12,17.5522847 12,17 C12,16.4477153 11.5522847,16 11,16 L8,16 Z"
+                                                    fill="#000000" fill-rule="nonzero"></path>
 											</g>
 										</svg>
                                         <!--end::Svg Icon-->
