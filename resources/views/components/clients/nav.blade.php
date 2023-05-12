8fb5986a3c2c5ca1c762758a586633cf441a5dec
jules fabien <julesfabien96@gmail.com>
2023-05-12 11:46:35 +0200

    fix

--- resources/views/components/clients/nav.blade.php
+++ resources/views/components/clients/nav.blade.php
@@ -23,41 +23,12 @@
                         <span>Pay with MOMO</span>
                     </a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item mr-2">
                     <a class="nav-link d-flex tw-gap-1 align-items-center tw-text-l tw-font-semibold" href="#">
                         <span>Help</span>
                     </a>
                 </li>
 
-                <li class="nav-item dropdown">
-                    <a class="nav-link tw-text-l tw-font-semibold dropdown-toggle" href="#" role="button"
-                       data-toggle="dropdown"
-                       aria-expanded="false">
-                        {{auth('client')->user() ? auth('client')->user()->name : ''}}
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right tw-shadow tw-rounded">
-                        <a class="dropdown-item tw-text-l" href="#">
-                            <span class="ti ti-user tw-text-[20px]"></span>
-                            Profile
-                        </a>
-                        <a class="dropdown-item tw-text-lg" href="#">
-                            <span class="ti ti-settings-2 tw-text-[20px]"></span>
-                            Account
-                        </a>
-{{--                        <a class="dropdown-item tw-text-lg" href="#">--}}
-{{--                            <span class="ti ti-square-asterisk tw-text-[20px]"></span>--}}
-{{--                            Change Password--}}
-{{--                        </a>--}}
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item tw-text-lg" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                            <span class="ti ti-logout tw-text-[20px]"></span>
-                            Logout
-                        </a>
-                        <form id="logout-form" action="{{route('client.logout')}}" method="POST" class="d-none">
-                            @csrf
-                        </form>
-                    </div>
-                </li>
                 <li class="nav-item">
                     <a class="nav-link d-flex tw-gap-1 align-items-center btn btn-accent btn-sm text-dark px-4 rounded-sm tw-font-semibold " href="#">
                         <span>Check Bills</span>
