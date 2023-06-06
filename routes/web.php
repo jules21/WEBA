a87a0a39b94d8e805cc980feea7c0b9948072449
jabogit <jabojeanjosue@gmail.com>
2023-06-06 10:35:14 +0200

    issue rorting changes

--- routes/web.php
+++ routes/web.php
@@ -411,12 +411,12 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'auth'], fu
     });
 
     Route::group(['prefix' => 'issues', 'as' => 'issues.'], function () {
-        Route::get('/reported', [\App\Http\Controllers\IssueReportController::class, 'reportedIssues'])
+        Route::get('/reported', [IssueReportController::class, 'reportedIssues'])
             ->name('reported');
         Route::put('/{issueReport}/reply', [ReplyIssueReportController::class, 'replyIssue'])
             ->name('reply');
-        Route::get('/issue-reporting',[App\Http\Controllers\IssuesReportingContrller::class,'reportingIssues'])->name('issues.reporting');
-        Route::post('/issue-reporting/store',[App\Http\Controllers\IssuesReportingContrller::class,'store'])->name('issue.reporting.store');
+        Route::get('/issue-reporting', [App\Http\Controllers\IssuesReportingContrller::class, 'reportingIssues'])->name('issues.reporting');
+        Route::post('/issue-reporting/store', [App\Http\Controllers\IssuesReportingContrller::class, 'store'])->name('issue.reporting.store');
     });
 
 });
